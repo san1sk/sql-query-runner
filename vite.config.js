d2f2ea509fed54ecf462b123286b259960f6eb18
Sanchit Khokhar <sanchitkhokhar1.sk@gmail.com>
2025-03-31 20:20:30 +0530

    Github pages build config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sql-query-runner/',
   server: {
     // Add proper CORS headers
     cors: true
